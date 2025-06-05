2a882833b5b2f7d885676003f0c22bd64f55ab99
joadio2 <joaquin.dionicioo@gmail.com>
2025-06-05 12:28:42 +0100

    error message

--- src/pages/failed.tsx
+++ src/pages/failed.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
 import styles from "./Failed.module.css"; // Assuming you use this file
 import { useNavigate } from "react-router-dom";
 export default function Failed() {
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState(
-    "An error occurred during processing. Please try again later."
-  );
 
   const handleRetry = () => {
     navigate("/");
@@ -15,7 +11,9 @@ export default function Failed() {
     <section className={styles.container}>
       <div className={styles.titleContainer}>
         <h1 className={styles.title}>Something Went Wrong</h1>
-        <p className={styles.message}>{errorMessage}</p>
+        <p className={styles.message}>
+          An error occurred during processing. Please try again later.
+        </p>
       </div>
 
       <div className={styles.resultSection}>
